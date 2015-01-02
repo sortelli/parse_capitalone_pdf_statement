b22a8821821ac13234b03d0d95a88ae8e1980057
Joe Sortelli <joe@sortelli.com>
2015-01-01 16:22:33 -0800

    Sort transactions by trx number

--- parse_capitalone_pdf_statement.rb
+++ parse_capitalone_pdf_statement.rb
@@ -5,16 +5,39 @@ class CapitalOneStatement
   TRANSACTION_REGEX = /^(\d+) +(\d\d) ([A-Z][A-Z][A-Z]) (.+[^ ]) +(\(?\$[0-9,.]+\)?) *$/
 
   def initialize(pdf_path)
-    @transactions       = []
-    @payments           = []
     @previous_balance   = nil
     @total_payments     = nil
     @total_fees         = nil
     @total_transactions = nil
     @new_balance        = nil
+    @payments           = []
+    @transactions       = []
 
-    @reader = PDF::Reader.new(pdf_path)
-    @reader.pages.each_with_index do |page, page_num|
+    parse_from_pdf pdf_path
+
+    sort_by_trx_num = lambda {|trx| trx[:number]}
+
+    @payments     = @payments    .sort_by &sort_by_trx_num
+    @transactions = @transactions.sort_by &sort_by_trx_num
+  end
+
+  def to_json
+    JSON.pretty_generate({
+      :previous_balance   => @previous_balance,
+      :total_payments     => @total_payments,
+      :total_fees         => @total_fees,
+      :total_transactions => @total_transactions,
+      :new_balance        => @new_balance,
+      :payments           => @payments,
+      :transactions       => @transactions
+    })
+  end
+
+  private
+
+  def parse_from_pdf(pdf_path)
+    reader = PDF::Reader.new(pdf_path)
+    reader.pages.each_with_index do |page, page_num|
       page.text.split("\n").each do |line|
         if @previous_balance.nil?
           amount_strs = line.scan(/\$[\d,.]+/)
@@ -45,25 +68,11 @@ class CapitalOneStatement
     end
   end
 
-  def to_json
-    JSON.pretty_generate({
-      :previous_balance   => @previous_balance,
-      :total_payments     => @total_payments,
-      :total_fees         => @total_fees,
-      :total_transactions => @total_transactions,
-      :new_balance        => @new_balance,
-      :payments           => @payments,
-      :transactions       => @transactions
-    })
-  end
-
-  private
-
   def parse_transaction(line)
     return nil unless line =~ TRANSACTION_REGEX
 
     {
-      :number     => $1,
+      :number     => $1.to_i,
       :day        => $2,
       :month      => $3,
       :desc       => $4,
